513b3bc1ef77a941040beb192bf5c4890adace0f
Gerzon <gerzon.bautista@dafiti.com.co>
2021-09-30 19:42:06 -0500

    Se integran los microservicios con el certificador

--- microservice_two/app.py
+++ microservice_two/app.py
@@ -1,10 +1,29 @@
 from flask import Flask
 import ssl
+import requests
 
 
 app = Flask(__name__)
-ssl_context = ssl.create_default_context(purpose=ssl.Purpose.CLIENT_AUTH, cafile='./ca.crt')
-ssl_context.load_cert_chain(certfile='./ca.crt', keyfile='./ca.key', password=None)
+
+
+def write_file(name_file, content_file):
+    text_file = open("./certificates/%s" % name_file, "w")
+    text_file.write(content_file)
+    text_file.close()
+
+
+def get_certificate():
+    response = requests.get('http://127.0.0.1:1340/certification')
+    certificate = response.json()
+    write_file('ca.crt', certificate['crt'])
+    write_file('ca.key', certificate['key'])
+    write_file('ca.pem', certificate['pem'])
+
+
+# set ssl_context
+get_certificate()
+ssl_context = ssl.create_default_context(purpose=ssl.Purpose.CLIENT_AUTH, cafile='./certificates/ca.crt')
+ssl_context.load_cert_chain(certfile='./certificates/ca.crt', keyfile='./certificates/ca.key', password=None)
 ssl_context.verify_mode = ssl.CERT_REQUIRED
 
 
