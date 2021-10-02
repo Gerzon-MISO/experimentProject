336c94b62d025e4b2eaa116bcfe945edd49444d8
Gerzon <gerzon.bautista@dafiti.com.co>
2021-10-01 22:45:39 -0500

    Integración con microservicio pacientes

--- microservice_servicios/app.py
+++ microservice_servicios/app.py
@@ -1,25 +1,17 @@
-from flask.views import http_method_funcs
-from  microservice_servicios import create_app
-from flask_restful import Resource,Api
+from flask_restful import Resource, Api
 from flask import Flask, request
 import requests
 import json
 from faker import Faker
 from random import randrange
-import  random
+import random
 import ssl
 import http.client
 
 app = Flask(__name__)
-api=Api(app)
-
-# app=create_app('default')
-# app_context = app.app_context()
-# app_context.push()
-
-# api=Api(app)
-
+api = Api(app)
 fake = Faker()
+
 
 def write_file(name_file, content_file):
     text_file = open("./certificates/%s" % name_file, "w")
@@ -41,32 +33,32 @@ ssl_context = ssl.create_default_context(purpose=ssl.Purpose.CLIENT_AUTH, cafile
 ssl_context.load_cert_chain(certfile='./certificates/ca.crt', keyfile='./certificates/ca.key', password=None)
 ssl_context.verify_mode = ssl.CERT_REQUIRED
 
-class Servicios(Resource):
+
+class Services(Resource):
+
     def get(self):
-
-        payload = {'identificacion': request.json['identificacion']}
-
+        payload = {"identificacion": request.json['identificacion']}
+        json_data = json.dumps(payload)
         context = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
         context.load_cert_chain(certfile='./certificates/ca.pem')
         connection = http.client.HTTPSConnection('127.0.0.1', port=1338, context=context)
-        connection.request(method="GET", url='/paciente')
-        content = connection.getresponse()
+        connection.request(method="GET", url='/paciente', body=json_data, headers={'Content-type': 'application/json'})
+        response = connection.getresponse()
+        paciente = response.read()
+        my_json = paciente.decode('utf8').replace("'", '"')
+        data = json.loads(my_json)
 
-        #content = requests.get('http://127.0.0.1:1338/paciente', json=payload)
+        servicios = ["cirugia", "examenes", "imagenes", "inyectologia", "medicina interna", "neurologia","traumatologia","hematologia","infectologia"]
+        lista_servicios = []
 
-        servicios=["cirugia", "examenes", "imagenes", "inyectologia", "medicina interna", "neurologia","traumatologia","hematologia","infectologia"]
-
-        paciente = content.json()
-
-        servicioPaciente={}
-        listaServicio=[]
-        for ser in range(1,randrange(1,10)):
-            servicioPaciente = {"idServ" : ser, "servicio":random.choice(list(servicios))}        
-            listaServicio.append(servicioPaciente)
+        for ser in range(1, randrange(1, 10)):
+            servicio_paciente = {"idServ": ser, "servicio": random.choice(list(servicios))}
+            lista_servicios.append(servicio_paciente)
         
-        return {"identificacion":paciente['identificacion'],  "servicios":listaServicio}
+        return {"paciente": data["identificacion"],  "servicios": lista_servicios}
 
-api.add_resource(Servicios, '/servicios')
+
+api.add_resource(Services, '/servicios')
 
 if __name__ == '__main__':
     app.run(port=1337, debug=True, ssl_context=ssl_context)
